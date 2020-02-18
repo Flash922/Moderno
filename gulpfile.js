4954ef6e25bd13b851bcf85fd97944c1c2894823
Flash922 <Flash92@gmail.com>
2020-02-18 03:37:01 +0300

    add filter elements mixitup

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ let gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     cssmin = require('gulp-cssmin');
 
+
 gulp.task('sass', function(){
   return gulp.src('app/scss/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}))
@@ -20,22 +21,24 @@ gulp.task('sass', function(){
 
 gulp.task('style', function(){
   return gulp.src([
+    'node_modules/normalize.css/normalize.css',
     'node_modules/slick-carousel/slick/slick.css',
     'node_modules/magnific-popup/dist/magnific-popup.css'
-])
-    .pipe(concat('libs.min.js'))
-    .pipe(cssmin())
-    .pipe(gulp.dest('app/css'))
+  ])
+      .pipe(concat('libs.min.css'))
+      .pipe(cssmin())
+      .pipe(gulp.dest('app/css'))
 });
 
 gulp.task('script', function(){
   return gulp.src([   
     'node_modules/slick-carousel/slick/slick.js',
-    'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
-])
-  .pipe(concat('libs.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('app/js'))
+    'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+    'node_modules/mixitup/dist/mixitup.js'
+  ])
+      .pipe(concat('libs.min.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('app/js'))
 });
 
 gulp.task('html', function(){
