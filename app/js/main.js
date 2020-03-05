8c8dbdef6483920da7e73b3e33ce0984c05ee1a0
Flash922 <Flash92@gmail.com>
2020-03-05 21:44:15 +0300

    add new items.

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,24 @@
 $(function(){
+  
+  
 
-  var mixer = mixitup('.products__inner-box');
+  // //код автозакрытия выпадающих меню
+  // $(".all__product-page").on("click", function(e) {
+  //   e.preventDefault();
+  //   $(".all__products").slideToggle(200);
+  //   $(document).mouseup(function(e) {
+  //     // событие клика по веб-документу
+  //     let div = $(".all__product-page"); // тут указываем ID элемента
+  //     if (
+  //       !div.is(e.target) && // если клик был не по нашему блоку
+  //       div.has(e.target).length === 0
+  //     ) {
+  //       $(".all__products").hide(100); // скрываем его
+  //     }
+  //   });
+  // });
 
-  $(".rate-star").rateYo({
+  $('.rate-star').rateYo({
     rating: 5,
     starWidth: "15px",
     readOnly: true
@@ -10,9 +26,22 @@ $(function(){
 
   $('.product-slider__inner').slick({
     dots: true,
-    arrows: false,
+    prevArrow: false,
+    nextArrow: false,
     slidesToShow: 4,
     slidesToScroll: 4
   });
+
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 100,
+    to: 600,
+    prefix: "$"
+  });
+
+  var mixer = mixitup('.products__inner-box');
+
 
 })
